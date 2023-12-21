3e55a64531f6460e3d0476a2ff3babb63296c089
ankan5960 <ankanmaity27@gmail.com>
2023-12-22 03:55:15 +0530

    Day1 commited

---
+++ day1.js
@@ -0,0 +1,8 @@
+// length of a string
+
+var arr="hellob vcbcvb sdbhbvvhsdbhsdbv xbvu uvgsdvvb hvgygvys sdjhshsdifbidksjdjsdn"
+
+var maxlen=250;
+console.log("you write "+(arr.length)+"chars and you have "+(maxlen-arr.length)+"char left");
+
+//slice funtion
