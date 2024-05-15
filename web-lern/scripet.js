fc81f06feab7ed6691003c9aaaa31a5254a0617e
ankan5960 <ankanmaity27@gmail.com>
2024-05-15 20:57:00 +0530

    Day3

--- web-lern/scripet.js
+++ web-lern/scripet.js
@@ -1,80 +1,71 @@
+var patner1="";
+
+const femalepatner = ["Ankita","Subhasree","Smriti","Anushka","Srija","Kankana","Moumita","Somaroti","Jenifa","Priyanka","Pratysha","Sampreeti","Shuhani","Soni","Purba","Poushali","Neha"];
+const malepatnerpatner = ["Shuven","Rohan","Tanmoy","Writ","Jeet","Devraj","Subhadeep","Abhijaan","Abhik","Ayan","Abhradeep","Aritra","Binayak","Debojyoti","Joti","Manojit","Sujoy","Suprodip",];
+ 
 function patnerName(gender,name){
-    let number=Math.floor(Math.random() * (10 - 1 + 1)) + 1;
-    switch(number){
-        case 1 :if(gender=="M"){
-                    console.log(name+" + Sophia");
-                }
-                else{
-                    console.log(name+" + Noah");
-                } 
-                break;
-        case 2 :if(gender=="M"){
-                    console.log(name+" + Olivia");
-                }
-                else{
-                    console.log(name+" + Elijah");
-                } 
-                break;
-        case 3 :if(gender=="M"){
-                    console.log(name+" + Emma");
-                }
-                else{
-                    console.log(name+" + Oliver");
-                } 
-                break;
-        case 4 :if(gender=="M"){
-                    console.log(name+" + Ava");
-                }
-                else{
-                    console.log(name+" + James");
-                } 
-                break;
-        case 5 :if(gender=="M"){
-                    console.log(name+" + Charlotte");
-                }
-                else{
-                    console.log(name+" + Benjamin");
-                } 
-                break;
-        case 6 :if(gender=="M"){
-                    console.log(name+" + Isabella");
-                }
-                else{
-                    console.log(name+" + Lucas");
-                } 
-                break;
-        case 7 :if(gender=="M"){
-                    console.log(name+" + Mia");
-                }
-                else{
-                    console.log(name+" + Henry");
-                } 
-                break;
-        case 8 :if(gender=="M"){
-                    console.log(name+" + Amelia");
-                }
-                else{
-                    console.log(name+" + Alexander");
-                } 
-                break;
-        case 9 :if(gender=="M"){
-                    console.log(name+" + Evelyn");
-                }
-                else{
-                    console.log(name+" + William");
-                } 
-                break;
-        case 10 :if(gender=="M"){
-                    console.log(name+" + Harper");
-                }
-                else{
-                    console.log(name+" + Liam");
-                } 
-                break;
-        default: console.log("Sorry your patner still not born");
-                break;
+    let number=Math.floor(Math.random() * (18 - 1 + 1)) + 1;
+    console.log(number);
+    if(gender=='M'){
+        patner1=femalepatner[number];
+    }
+    else{
+        patner1=malepatnerpatner[number];
     }
 }
+
+var inputname=document.getElementById("inputName");
+var patnerBox=document.getElementById('patnerBox');
+var male=document.getElementById('MaleId');
+var female=document.getElementById('FemaleId');
+
+
+function GetName(){
+    //patnerName()
+    var gender="";
+    if(male.checked && !female.checked){
+        gender="M";
+    }
+    else if(!male.checked && female.checked){
+        gender="F";
+    }
+    else if((male.checked && female.checked)||(!male.checked && !female.checked)){
+        window.alert("Checked gender");
+        return ;
+    }
+    patnerName(gender,inputname.value);
+        if(!inputname.value){
+            window.alert("Enter name first");
+            return;
+        }
+        else if(!patner1){
+            patnerBox.innerHTML=`${inputname.value} you always be single `;
+            patnerBox.style.display="block";
+        }
+        else{
+            patnerBox.innerHTML=`${patner1} + ${inputname.value}`;
+            patnerBox.style.display="block";
+        }
+};
+
+function reponsive(){
+    var w = window.innerWidth;
+    var h = window.innerHeight;
+    if(w<=615 && w>559){
+        patnerBox.style.height="70px";
+        patnerBox.style.fontSize = "xx-large";
+    }
+    else if(w<=559 && w>270){
+        patnerBox.style.fontSize = "small";
+        patnerBox.style.height="20px";
+    }
+    else if(w<=269){
+        patnerBox.style.height="40px";
+    }
+    console.log(w,h);
+}
+
+reponsive();
 
 //patnerName("M", "ankan");
 
@@ -97,7 +88,23 @@ function patnerName(gender,name){
 
 
 
-let h1tag=document.querySelector("#div1");
+//var id=document.getElementById('div1');
+//id.innerHTML="Kannakan";
 
-h1tag.innerHTML="Kankana";
+// var classname=document.getElementsByClassName('divtype');
+// classname[2].innerHTML="ankan"
+
+// var query=document.querySelector(".divtype");
+// query.innerHTML="ggg";
+
+// var quary=document.querySelectorAll(".divtype");
+// quary[2].innerHTML="fff";
+
+// console.log(quary);
+
+// var tagname=document.getElementsByTagName('h1');
+
+// console.log(tagname);
+
+// tagname[0].innerHTML ="ankan";
 
